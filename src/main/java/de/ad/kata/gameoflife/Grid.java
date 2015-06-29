b7030cb9da616cddffeddf8723b57d2113ecc7b5
a11n <andre.diermann@gmail.com>
2015-06-29 10:58:19 +0200

    Made first test green.

--- src/main/java/de/ad/kata/gameoflife/Grid.java
+++ src/main/java/de/ad/kata/gameoflife/Grid.java
@@ -1,9 +1,24 @@
 package de.ad.kata.gameoflife;
 
+import java.util.Arrays;
+
 public class Grid {
   private final boolean [][] grid;
   
   public Grid(int size) {
     grid = new boolean[size][size];
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    Grid grid1 = (Grid) o;
+
+    return Arrays.deepEquals(grid, grid1.grid);
+  }
+
+  @Override public int hashCode() {
+    return Arrays.deepHashCode(grid);
   }
 }
