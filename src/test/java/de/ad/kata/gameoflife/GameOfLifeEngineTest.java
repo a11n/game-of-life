46d6f05089b1c4cb06e33dbf2dff82c24f07e567
a11n <andre.diermann@gmail.com>
2015-06-29 12:21:41 +0200

    Added test for second rule.

--- src/test/java/de/ad/kata/gameoflife/GameOfLifeEngineTest.java
+++ src/test/java/de/ad/kata/gameoflife/GameOfLifeEngineTest.java
@@ -1,10 +1,19 @@
 package de.ad.kata.gameoflife;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class GameOfLifeEngineTest {
+
+  private GameOfLifeEngine engine;
+
+  @Before
+  public void setUp() throws Exception {
+    engine = new GameOfLifeEngine();
+  }
+
   /**
    * Rule 1:
    * Any live cell with fewer than two live neighbors dies, as if caused by under-population.
@@ -14,11 +23,25 @@ public class GameOfLifeEngineTest {
   @Test
   public void testFirstRule() throws Exception {
     Grid grid = initGridWithOneLivingCellAndOneLivingNeighbor();
-    GameOfLifeEngine engine = new GameOfLifeEngine();
     
     Grid nextGeneration = engine.computeNextGeneration(grid);
     
     assertTrue(nextGeneration.cellAt(1,1).isDead());
+  }
+
+  /**
+   * Rule 2:
+   * Any live cell with two or three live neighbours lives on to the next generation.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testSecondRule() throws Exception {
+    Grid grid = initGridWithOneLivingCellAndThreeLivingNeighbors();
+
+    Grid nextGeneration = engine.computeNextGeneration(grid);
+
+    assertTrue(nextGeneration.cellAt(1, 1).isAlive());
   }
 
   private Grid initGridWithOneLivingCellAndOneLivingNeighbor() {
@@ -27,6 +50,16 @@ public class GameOfLifeEngineTest {
     grid.cellAt(1,1).reproduce(); //living cell
     grid.cellAt(0,0).reproduce(); //living neighbor
     
+    return grid;
+  }
+
+  private Grid initGridWithOneLivingCellAndThreeLivingNeighbors() {
+    Grid grid = new Grid(9);
+
+    grid.cellAt(1,1).reproduce(); //living cell
+    grid.cellAt(0,2).reproduce(); //living neighbor
+    grid.cellAt(2,0).reproduce(); //living neighbor
+
     return grid;
   }
 }
