3ddb7a1029f302dd11b740c77b2ead4111aeecb5
a11n <andre.diermann@gmail.com>
2015-06-29 12:41:06 +0200

    Added test for fourth rule.

--- src/test/java/de/ad/kata/gameoflife/GameOfLifeEngineTest.java
+++ src/test/java/de/ad/kata/gameoflife/GameOfLifeEngineTest.java
@@ -58,6 +58,21 @@ public class GameOfLifeEngineTest {
 
     assertTrue(nextGeneration.cellAt(1,1).isDead());
   }
+
+  /**
+   * Rule 4:
+   * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+   *
+   * @throws Exception
+   */
+  @Test
+  public void testFourthRule() throws Exception {
+    Grid grid = initGridWithOneDeadCellAndThreeLivingNeighbors();
+
+    Grid nextGeneration = engine.computeNextGeneration(grid);
+
+    assertTrue(nextGeneration.cellAt(1,1).isAlive());
+  }
   
   private Grid initGridWithOneLivingCellAndOneLivingNeighbor() {
     Grid grid = new Grid(3);
@@ -72,6 +87,17 @@ public class GameOfLifeEngineTest {
     Grid grid = new Grid(3);
 
     grid.cellAt(1,1).reproduce(); //living cell
+    grid.cellAt(0,0).reproduce(); //living neighbor
+    grid.cellAt(0,2).reproduce(); //living neighbor
+    grid.cellAt(2,0).reproduce(); //living neighbor
+
+    return grid;
+  }
+
+  private Grid initGridWithOneDeadCellAndThreeLivingNeighbors() {
+    Grid grid = new Grid(3);
+
+    grid.cellAt(1,1).die(); //dead cell
     grid.cellAt(0,0).reproduce(); //living neighbor
     grid.cellAt(0,2).reproduce(); //living neighbor
     grid.cellAt(2,0).reproduce(); //living neighbor
